2403f8d564784bfbfa93fcb3a98027060b9b0268
simon8491 <chmel.simon@gmail.com>
2024-02-05 11:38:20 +0100

    #

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Solution solution = new MoveZeros();
+        Solution solution = new Solution();
         int[]nums = {0,1,0,3,12};
         solution.moveZeros(nums);
     }
