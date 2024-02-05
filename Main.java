3e699f27f684773b5688b07250d08e5ff9ed8344
simon8491 <chmel.simon@gmail.com>
2024-02-05 11:48:57 +0100

    Delete main

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Solution solution = new Solution();
-        int[]nums = {0,1,0,3,12};
-        solution.moveZeros(nums);
-    }
-}
