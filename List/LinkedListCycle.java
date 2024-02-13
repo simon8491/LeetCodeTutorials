67c1e269957a036714883cb1ca9f867f7fe48671
simon8491 <chmel.simon@gmail.com>
2024-02-13 18:17:18 +0100

    Linked list cycle detection

---
+++ List/LinkedListCycle.java
@@ -0,0 +1,36 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+        this.next = null;
+    }
+}
+
+class LinkedListCycle {
+    public ListNode detectCycle(ListNode head)  {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slowPointer = head, fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {  // Find the meeting point using Floyd's Tortoise and Hare algorithm
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            
+            if (slowPointer == fastPointer) {
+                break;
+            }
+        }
+        if (fastPointer == null || fastPointer.next == null) {  // If there is no cycle
+            return null;
+        }
+        slowPointer = head;     // Move slow to the head and keep fast at the meeting point
+        while (slowPointer != fastPointer) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next;
+        }
+        return slowPointer;
+    }
+}
