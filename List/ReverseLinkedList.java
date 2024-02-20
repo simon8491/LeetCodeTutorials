ad5a5f6b9fb9a7bc3948c50a1c755234742c8fdd
simon8491 <chmel.simon@gmail.com>
2024-02-20 16:03:50 +0100

    #

--- List/ReverseLinkedList.java
+++ List/ReverseLinkedList.java
@@ -13,8 +13,8 @@ class LinkedListReverse {
         ListNode prev = null;       // the node before the head node
         
         while (head != null) {
-            ListNode next = head.next;  // storing tha value of the next node
-            head.next = prev;   // to reverse the pointer; make it to point to originaly previous node but consider it as the next
+            ListNode next = head.next;  // storing a value of the next node
+            head.next = prev;   // to reverse the pointer; make it to point to the originaly previous node but consider it as the next
             // 1->2->3->4->5... null<-1 2->3->4->5... 2<-1 3->4->5... 3<-2<-1 4->5... 4<-3<-2<-1 5->null... 5<-4<-3<-2<-1<-null...
             prev = head;
             head = next;
