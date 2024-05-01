baa5d38eecd31d8c7bbc821c03596bf8172c35b0
simon8491 <chmel.simon@gmail.com>
2024-05-01 22:20:48 +0200

    Longest common prefix

---
+++ Arrays&Strings/LongestComPrefix.java
@@ -0,0 +1,18 @@
+class LongestComPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {  //indexOf can treturn only -1 or 0.
+                //Since flower as a whole is not in flow, it return -1 and  prefix=prefix.substring(0,prefix.length()-1) reduces prefix to "flowe".
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
