b48399ce999379ca3b8169468b165b379c0e5e2c
simon8491 <144045360+simon8491@users.noreply.github.com>
2024-05-04 19:05:13 +0200

    Update LongestComPrefix.java

--- Arrays&Strings/LongestComPrefix.java
+++ Arrays&Strings/LongestComPrefix.java
@@ -5,7 +5,7 @@ class LongestComPrefix {
         }
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {  //indexOf can treturn only -1 or 0.
+            while (strs[i].indexOf(prefix) != 0) {  //indexOf can return only -1 or 0.
                 //Since flower as a whole is not in flow, it return -1 and  prefix=prefix.substring(0,prefix.length()-1) reduces prefix to "flowe".
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) {
