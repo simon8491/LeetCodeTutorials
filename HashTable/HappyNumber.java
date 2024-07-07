17ee22045fb48eae8699b7d6f1268077a65be93f
simon8491 <chmel.simon@gmail.com>
2024-07-07 15:38:57 +0200

    //

--- HashTable/HappyNumber.java
+++ HashTable/HappyNumber.java
@@ -5,7 +5,7 @@ class HappyNumber {
     public static boolean isHappy(int n) {
         Set<Integer> loopCheck = new HashSet<>();
         while (n != 1 && !loopCheck.contains(n))    {
-            loopCheck.add(n);
+            loopCheck.add(n);   // needs to be added to Set before splitting the digits
             int sumToSquare = 0;    // reinitialize by 0 in every cycle since the sumToSquare is moved to n
             while (n != 0) {    // split the multi-digit number
                 sumToSquare += (n%10)*(n%10);   // get the last digit and square it
