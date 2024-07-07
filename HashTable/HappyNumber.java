1ce1a59bd93845f38a946b4b40a91e20f7d40652
simon8491 <chmel.simon@gmail.com>
2024-07-07 15:36:53 +0200

    Is happy number

---
+++ HashTable/HappyNumber.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class HappyNumber {
+    public static boolean isHappy(int n) {
+        Set<Integer> loopCheck = new HashSet<>();
+        while (n != 1 && !loopCheck.contains(n))    {
+            loopCheck.add(n);
+            int sumToSquare = 0;    // reinitialize by 0 in every cycle since the sumToSquare is moved to n
+            while (n != 0) {    // split the multi-digit number
+                sumToSquare += (n%10)*(n%10);   // get the last digit and square it
+                n /= 10;                        // get the first digit
+            }
+            n = sumToSquare;   
+        }
+        return n==1;    // if(n==1) return true
+    }
+    
+}
