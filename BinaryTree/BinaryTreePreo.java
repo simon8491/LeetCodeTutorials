94990b3bc55ad570abe1c7b5a74fd66175771c60
simon8491 <chmel.simon@gmail.com>
2024-08-11 14:14:17 +0200

    Binary Tree preorder traversal

---
+++ BinaryTree/BinaryTreePreo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreePreo {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root != null)   {
+            result.add(root.val);
+            result.addAll(preorderTraversal(root.left));
+            result.addAll(preorderTraversal(root.right));
+        }
+        return result;
+    }
+}
